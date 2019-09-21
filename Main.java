bfdc5b4a919509ca1f4083fc44f3983a4a1488e0
kterry1 <40950641+kterry1@users.noreply.github.com>
2019-09-21 01:22:15 -0400

    Added limits to inputs

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package com.codewithkevin;
 
 import com.sun.security.jgss.GSSUtil;
+import org.w3c.dom.ls.LSOutput;
 
 import java.awt.*;
 import java.text.NumberFormat;
@@ -13,21 +14,44 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        byte periodInYears = 0;
+        int periodInMonths = 0;
+
         Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte periodInYears = scanner.nextByte();
-        int periodInMonths = periodInYears * (int)MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Period (Years): ");
+            periodInYears = scanner.nextByte();
+            if(periodInYears >=1 && periodInYears <= 30){
+                periodInMonths = periodInYears * (int)MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         float total = (principal * ((monthlyInterest * (float)Math.pow(1 + monthlyInterest, periodInMonths)) / ((float)Math.pow(1 + monthlyInterest, periodInMonths) - 1)));
         String mortgage = NumberFormat.getCurrencyInstance(Locale.US).format(total);
         System.out.print("Mortgage: " + mortgage);
+
+
+
  }
 }
