2866b8547b755229692cd9ae3c75811e66983f26
Kevin <terrykevin1@gmail.com>
2019-09-20 22:46:13 -0400

    Removed unneccesary imports

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ import com.sun.security.jgss.GSSUtil;
 
 import java.awt.*;
 import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
